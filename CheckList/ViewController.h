cbedfda7dd8dcde90a649a0d70118c49ed40cbd5
Michael <liukaiyi54@gmail.com>
2015-09-02 11:55:41 +0800

    Initial Commit

---
+++ CheckList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckList
+//
+//  Created by Michael on 9/2/15.
+//  Copyright (c) 2015 Michael's None-Exist Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CheckList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CheckList
+//
+//  Created by Michael on 9/2/15.
+//  Copyright (c) 2015 Michael's None-Exist Company. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
