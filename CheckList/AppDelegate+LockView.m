227e3729428e92cb883204a3a46ee51e757f6305
Michael <liukaiyi54@gmail.com>
2015-10-03 13:18:57 +0800

    delete the delay

--- CheckList/AppDelegate+LockView.m
+++ CheckList/AppDelegate+LockView.m
@@ -18,12 +18,8 @@
 - (void)showGestureLockView {
     LoginViewController *vc = [[LoginViewController alloc] init];
  //        vc.unlockGesture = YES;
-    double delayInSeconds = 0.5;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds *   NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        [self.window.rootViewController presentViewController:vc animated:NO completion:nil];
-    });
-    //[self.window.rootViewController presentViewController:vc animated:NO completion:nil];
+    
+    [self.window.rootViewController presentViewController:vc animated:YES completion:nil];
 }
 
 @end
