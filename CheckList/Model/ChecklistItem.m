58a2d72a37982d973d9d4ab12a5ec55e8ac8cbc5
Michael <liukaiyi54@gmail.com>
2015-11-26 15:16:39 +0800

    代码对齐

--- CheckList/Model/ChecklistItem.m
+++ CheckList/Model/ChecklistItem.m
@@ -31,11 +31,11 @@
 //This method is the initializer for all archived objects.
 - (id)initWithCoder:(NSCoder *)aDecoder {
     if ((self = [super init])) {
-        self.text = [aDecoder decodeObjectForKey:@"Text"];
-        self.checked = [aDecoder decodeBoolForKey:@"Checked"];
-        self.dueDate = [aDecoder decodeObjectForKey:@"DueDate"];
+        self.text         = [aDecoder decodeObjectForKey:@"Text"];
+        self.checked      = [aDecoder decodeBoolForKey:@"Checked"];
+        self.dueDate      = [aDecoder decodeObjectForKey:@"DueDate"];
         self.shouldRemind = [aDecoder decodeBoolForKey:@"ShouldRemind"];
-        self.itemId = [aDecoder decodeIntegerForKey:@"ItemID"];
+        self.itemId       = [aDecoder decodeIntegerForKey:@"ItemID"];
     }
     return self;
 }
@@ -46,11 +46,11 @@
 
 //Encodes the receiver using a given archiver.
 - (void)encodeWithCoder:(NSCoder *)aCoder {
-    [aCoder encodeObject:self.text forKey:@"Text"];
-    [aCoder encodeBool:self.checked forKey:@"Checked"];
-    [aCoder encodeObject:self.dueDate forKey:@"DueDate"];
+    [aCoder encodeObject:self.text       forKey:@"Text"];
+    [aCoder encodeBool:self.checked      forKey:@"Checked"];
+    [aCoder encodeObject:self.dueDate    forKey:@"DueDate"];
     [aCoder encodeBool:self.shouldRemind forKey:@"ShouldRemind"];
-    [aCoder encodeInteger:self.itemId forKey:@"ItemID"];
+    [aCoder encodeInteger:self.itemId    forKey:@"ItemID"];
 }
 
 - (UILocalNotification *)notificationForThisItem {
@@ -73,11 +73,11 @@
     if (self.shouldRemind && [self.dueDate compare:[NSDate date]] != NSOrderedAscending) {
         UILocalNotification *localNotification = [[UILocalNotification alloc] init];
         
-        localNotification.fireDate = self.dueDate;
-        localNotification.timeZone = [NSTimeZone defaultTimeZone];
+        localNotification.fireDate  = self.dueDate;
+        localNotification.timeZone  = [NSTimeZone defaultTimeZone];
         localNotification.alertBody = self.text;
         localNotification.soundName = UILocalNotificationDefaultSoundName;
-        localNotification.userInfo = @{@"ItemID": @"(self.itemId)"};
+        localNotification.userInfo  = @{@"ItemID": @"(self.itemId)"};
         
         [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
     }
